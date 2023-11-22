adbe3ea1e3ecebc02b8d602bb93604b9879a1e55
Bogdanko <bg.frol2112@gmail.com>
2023-11-22 15:48:46 +0300

    renamed:    elo copy.py -> elo+.py

--- elo+.py
+++ elo+.py
@@ -7,30 +7,13 @@ df_rating = df.assign(Count=float(0), Elo=float(1000))
 df_rating['FIO'] = df_rating['Name'] + df_rating['Surname']
 df_rating = df_rating[['FIO', 'Count', 'Elo']]
 
-filename = 'bio_rait_train.txt'
-lines = [line.rstrip('\n') for line in open(filename, encoding='utf-8')]
+# filename = 'bio_rait_train.txt'
+# lines = [line.rstrip('\n') for line in open(filename, encoding='utf-8')]
 
 # This is the parameter which makes "wins" more or less important
 # In normal chess games this is usually 20
 k = 45.0
-list = []
-for i in lines:
-    # Skip line if empty
-    if i.strip() == "": 
-        continue
-    splitted = i.split(",")
-    element_name = splitted[0].strip()
-    
-    if len(splitted) == 1:
-        count = 0.0
-        elo = 1000.0
-    else:
-        count = float(splitted[1].strip())
-        elo = float(splitted[2].strip())
-    i = [element_name, count, elo]
-    list.append(i)
-
-l = len(list)
+l = df.shape[0]
 try:
     while True:
         # Get two random elements
@@ -40,11 +23,11 @@ try:
             b = randint(0,l-1)
 
         # Найти элементы в списке по их id и посчитать их E
-        element_a = list[a]
-        element_b = list[b]
+        element_a = df_rating.iloc[a].to_list()
+        element_b = df_rating.iloc[b].to_list()
         # Это должно происходить внутри фунцкии get_random_num
-        R_a = element_a[2]
-        R_b = element_b[2]
+        R_a = float(element_a[2])
+        R_b = float(element_b[2])
         E_a = 1/(1 + (10**((R_b - R_a)/480)))
         E_b = 1/(1 + (10**((R_a - R_b)/480)))
         
@@ -53,34 +36,35 @@ try:
 
         if "1" in inpt:
             # Calculate Elo if 1 wins
-            element_a[2] = R_a + k*(1-E_a)
-            element_b[2] = R_b + k*(0-E_b)
+            df_rating.at[a, 'Elo'] = R_a + k*(1-E_a)
+            df_rating.at[b, 'Elo'] = R_b + k*(0-E_b)
             # Increment counter
-            element_a[1] += 1
-            element_b[1] += 1
+            df_rating.at[a, 'Count'] += 1
+            df_rating.at[b, 'Count'] += 1
         elif "2" in inpt:
             # Calculate elo if 2 wins
-            element_b[2] = R_b + k*(1-E_b)
-            element_a[2] = R_a + k*(0-E_a)
+            df_rating.at[b, 'Elo'] = R_b + k*(1-E_b)
+            df_rating.at[a, 'Elo'] = R_a + k*(0-E_a)
             # Increment counter
-            element_a[1] += 1
-            element_b[1] += 1
+            df_rating.at[a, 'Count'] += 1
+            df_rating.at[b, 'Count'] += 1
+
 
 # Остановка кода через - ctrl+c
 except KeyboardInterrupt as e:
-    True
+    print(df_rating.sort_values(by='Elo', ascending=False)) # Сортировка значений по убывани
 
-write_string = ''
-# Сортировка по рейтингу сверху вниз
-while list != []:
-    max = [0, 0, 0]
-    for i in list:
-        if max[2] < i[2]:
-            max = i
-    for i in max:
-        write_string += str(i)+", "
-    write_string += "\n"
-    list.remove(max)
-list_file = open(filename, "w", encoding='utf-8')
-list_file.write(write_string)
-list_file.close()
+# write_string = ''
+# # Сортировка по рейтингу сверху вниз
+# while list != []:
+#     max = [0, 0, 0]
+#     for i in list:
+#         if max[2] < i[2]:
+#             max = i
+#     for i in max:
+#         write_string += str(i)+", "
+#     write_string += "\n"
+#     list.remove(max)
+# list_file = open(filename, "w", encoding='utf-8')
+# list_file.write(write_string)
+# list_file.close()
